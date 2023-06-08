93844435f83a4b5a3c5766597ba31c3352e93a78
suryanarayananunna0612@gmail.com <suryanarayananunna0612@gmail.com>
2023-06-09 00:02:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// server.js
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
